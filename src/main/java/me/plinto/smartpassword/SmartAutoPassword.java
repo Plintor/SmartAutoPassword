a6916035bf79f440d8a60ef848fe848b7f6b6ec8
Plinto <106719262+Plintor@users.noreply.github.com>
2024-03-14 13:02:43 +0200

    Update SmartAutoPassword.java

--- src/main/java/me/plinto/smartpassword/SmartAutoPassword.java
+++ src/main/java/me/plinto/smartpassword/SmartAutoPassword.java
@@ -55,7 +55,7 @@ public class SmartAutoPassword {
             if (enabled) {
                 enabled = false;
                 updatecfg(false);
-                mc.ingameGUI.getChatGUI().printChatMessage(new TextComponentString(prefix+"\u0410\u0432\u0442\u043e\u041f\u0430\u0440\u043e\u043b\u044c \u0432\u044b\u043a\u043b\u044e\u0447\u0435\u043d"));
+                mc.ingameGUI.getChatGUI().printChatMessage(new TextComponentString(prefix+"\u0410\u0432\u0442\u043e\u041f\u0430\u0440\u043e\u043b\u044c \u0432\u044b\u043a\u043b\u044e\u0447\u0435\u043d")); // ... ... \u
             } else {
                 enabled = true;
                 updatecfg(true);
